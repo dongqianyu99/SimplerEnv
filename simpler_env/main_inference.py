aa041bdf8ab6daee61846573b07503461e0cd5e8
dongqianyu99 <dongqianyu99@gmail.com>
2026-05-17 01:14:53 +0800

    chore: lazy-load baseline policy dependencies

--- simpler_env/main_inference.py
+++ simpler_env/main_inference.py
@@ -1,18 +1,21 @@
 import os
 
 import numpy as np
-import tensorflow as tf
 
 from simpler_env.evaluation.argparse import get_args
 from simpler_env.evaluation.maniskill2_evaluator import maniskill2_evaluator
-from simpler_env.policies.octo.octo_server_model import OctoServerInference
-from simpler_env.policies.rt1.rt1_model import RT1Inference
 
-try:
-    from simpler_env.policies.octo.octo_model import OctoInference
-except ImportError as e:
-    print("Octo is not correctly imported.")
-    print(e)
+
+def _configure_tensorflow_memory(tf_memory_limit):
+    import tensorflow as tf
+
+    gpus = tf.config.list_physical_devices("GPU")
+    if len(gpus) > 0:
+        # prevent a single tf process from taking up all the GPU memory
+        tf.config.set_logical_device_configuration(
+            gpus[0],
+            [tf.config.LogicalDeviceConfiguration(memory_limit=tf_memory_limit)],
+        )
 
 
 if __name__ == "__main__":
@@ -21,32 +24,33 @@ if __name__ == "__main__":
     os.environ["DISPLAY"] = ""
     # prevent a single jax process from taking up all the GPU memory
     os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
-    gpus = tf.config.list_physical_devices("GPU")
-    if len(gpus) > 0:
-        # prevent a single tf process from taking up all the GPU memory
-        tf.config.set_logical_device_configuration(
-            gpus[0],
-            [tf.config.LogicalDeviceConfiguration(memory_limit=args.tf_memory_limit)],
-        )
 
     # policy model creation; update this if you are using a new policy model
     if args.policy_model == "rt1":
         assert args.ckpt_path is not None
+        _configure_tensorflow_memory(args.tf_memory_limit)
+        from simpler_env.policies.rt1.rt1_model import RT1Inference
+
         model = RT1Inference(
             saved_model_path=args.ckpt_path,
             policy_setup=args.policy_setup,
             action_scale=args.action_scale,
         )
     elif "octo" in args.policy_model:
+        _configure_tensorflow_memory(args.tf_memory_limit)
         if args.ckpt_path is None or args.ckpt_path == "None":
             args.ckpt_path = args.policy_model
         if "server" in args.policy_model:
+            from simpler_env.policies.octo.octo_server_model import OctoServerInference
+
             model = OctoServerInference(
                 model_type=args.ckpt_path,
                 policy_setup=args.policy_setup,
                 action_scale=args.action_scale,
             )
         else:
+            from simpler_env.policies.octo.octo_model import OctoInference
+
             model = OctoInference(
                 model_type=args.ckpt_path,
                 policy_setup=args.policy_setup,
